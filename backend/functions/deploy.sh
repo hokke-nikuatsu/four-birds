312b50ee122f8724c83ef477d306b3c17eacd066
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-10 17:09:50 +0000

    👷 Divide deployment process

---
+++ backend/functions/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ENV=$1
+CREDENTIALS_PATH=$2
+
+if [ "$ENV" = "dev" ]; then
+    echo "Deploying to development environment..."
+    cp .env.dev .env
+    PROJECT="four-birds-dev"
+elif [ "$ENV" = "prod" ]; then
+    echo "Deploying to production environment..."
+    cp .env.prod .env
+    PROJECT="four-birds-prod"
+else
+    echo "No environment is set."
+    exit 1
+fi
+
+export GOOGLE_APPLICATION_CREDENTIALS=$CREDENTIALS_PATH
+
+read -p "Do you continue? [y/N]: " CONFIRM
+if [[ $CONFIRM =~ ^[Yy]$ ]]; then
+    tsc
+    firebase deploy --only functions --project=$PROJECT
+else
+    echo "Deploying cancelled."
+    exit 1
+fi
+
+cleanup() {
+    echo "Cleaning up..."
+    rm -f .env
+    unset GOOGLE_APPLICATION_CREDENTIALS
+}
+
+trap cleanup EXIT
