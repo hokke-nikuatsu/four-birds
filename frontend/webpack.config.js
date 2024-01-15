c74df98b8bdd4d851f34f2f5ff1feeed8b5cbcb9
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-15 17:07:09 +0000

    👷 Add env setting

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -45,6 +46,9 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './public/index.html',
+		}),
+		new webpack.EnvironmentPlugin({
+			ENV: 'development',
 		}),
 	],
 	devServer: {
