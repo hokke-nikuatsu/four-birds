5ee6e20cc538b149496c5775e3ec4a16226e92b4
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-15 17:02:14 +0000

    👔 Add converter

--- backend/api/src/utils/common.ts
+++ backend/api/src/utils/common.ts
@@ -11,3 +11,22 @@ export const loadEnvProperty = (property: string): string => {
 	return value;
 };
 
+const toCamelCase = (s: string): string =>
+	s.replace(/_[a-z]/gi, (matched) => matched.charAt(1).toUpperCase());
+
+type CamelCase<S extends string> = S extends `${infer P}_${infer C}${infer R}`
+	? `${P}${Capitalize<C>}${CamelCase<R>}`
+	: S;
+
+export const convertToCamelCase = <T extends Record<string, unknown>>(
+	obj: T,
+): { [K in keyof T as CamelCase<K & string>]: T[K] } => {
+	const newObj: Record<string, unknown> = {};
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) {
+			const camelCaseKey = toCamelCase(key);
+			newObj[camelCaseKey] = obj[key];
+		}
+	}
+	return newObj as { [K in keyof T as CamelCase<K & string>]: T[K] };
+};
