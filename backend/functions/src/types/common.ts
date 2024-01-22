3b3c23377bb42f512857cad248c2b93a8499be81
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-22 01:38:16 +0000

    ⚰️ Remove unused config file

--- backend/functions/src/types/common.ts
+++
@@ -1,9 +0,0 @@
-export type FourBirdsConfig = {
-	'functions-region': string;
-	'newsdata-io-api-key': string;
-	'newsdata-io-api-url': string;
-	'db-url': string;
-	'db-name': string;
-	'db-user-name': string;
-	'db-password': string;
-};
