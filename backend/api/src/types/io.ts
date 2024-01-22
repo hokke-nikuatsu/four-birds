f66fd7487f14ac106457e43e9dbb734cbaa6dadf
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-22 14:02:13 +0000

    🏷️ Change types for I/O

--- backend/api/src/types/io.ts
+++ backend/api/src/types/io.ts
@@ -1,11 +1,25 @@
+export enum STATUS {
+	ERROR = 'error',
+	SUCCESS = 'success',
+}
+
+export type ApiResponse<T> = {
+	status: 'success' | 'error';
+	data?: T;
+	message?: string;
+};
+
+export type FetchNewsRequest = {
+	offset: string;
+};
+
 export type FetchNewsResponse = {
 	articleId: string;
 	title: string;
 	description: string;
-	publishedDate: Date;
+	publishedDate: string;
 	url: string;
 	ogpUrl: string;
 	publisher_name: string;
-	categories: string;
-	countries: string;
-};
+	categories: string[];
+}[];
