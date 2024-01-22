157057fa5823973dcbada08d4597925a373c3259
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-22 14:04:19 +0000

    👔 Format response values

--- backend/api/src/services/newsService.ts
+++ backend/api/src/services/newsService.ts
@@ -1,6 +1,14 @@
 import { type RowDataPacket, type PoolConnection } from 'mysql2/promise';
-import { type FetchNewsResponse } from 'types/io';
-import { convertToCamelCase } from '../utils/common';
+import { type DBFetchNews } from '../types/db';
+import { type FetchNewsResponse } from '../types/io';
+import {
+	capitalizeFirstLetter,
+	convertToCamelCase,
+	convertToJapanTime,
+	formatDate,
+	splitText,
+	trimText,
+} from '../utils/common';
 import { QUERY_SELECT_ARTICLES } from '../utils/query';
 
 export const fetchNewsList = async (
@@ -14,7 +22,22 @@ export const fetchNewsList = async (
 		throw new Error('Selection of news list failed.');
 	}
 
-	const newsList = rows.map(convertToCamelCase) as FetchNewsResponse[];
+	const newsList = rows.map(convertToCamelCase) as DBFetchNews;
 
-	return newsList;
+	const formattedNewsList = newsList.map((newsItem) => {
+		const convertedPublishedDate = convertToJapanTime(newsItem.publishedDate);
+		const formattedPublishedDate = formatDate(convertedPublishedDate);
+		const trimmedDescription = trimText(newsItem.description);
+		const splittedCategories = splitText(newsItem.categories);
+		const formattedCategories = capitalizeFirstLetter(splittedCategories);
+
+		return {
+			...newsItem,
+			description: trimmedDescription,
+			publishedDate: formattedPublishedDate,
+			categories: formattedCategories,
+		};
+	});
+
+	return formattedNewsList;
 };
