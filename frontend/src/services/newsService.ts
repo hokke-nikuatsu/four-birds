fd0453efb0e837ee795d42cb394d8ca5ee4ae42e
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-23 23:14:01 +0000

    🐛 Add timeout for fetch

--- frontend/src/services/newsService.ts
+++ frontend/src/services/newsService.ts
@@ -1,11 +1,14 @@
 import { type FetchNewsResponse, type ApiResponse, STATUS } from '../types/api';
+import { fetchApiAbortController, fetchApiTimeout } from '../utils/common';
 import { API_URL } from '../utils/environment';
 
 export const fetchNews = async (
 	newsCount: number,
 ): Promise<FetchNewsResponse> => {
 	try {
-		const response = await fetch(`${API_URL}/news?offset=${newsCount}`);
+		const response = await fetch(`${API_URL}/news?offset=${newsCount}`, {
+			signal: fetchApiAbortController.signal,
+		});
 
 		if (!response.ok) {
 			throw new Error(`Error: ${response.status}`);
@@ -23,7 +26,9 @@ export const fetchNews = async (
 
 		return fetchNewsResponse.data;
 	} catch (e) {
-		console.error('Fetching news failed:', e);
+		console.error('Fetching news failed:', JSON.stringify(e));
 		throw e;
+	} finally {
+		clearTimeout(fetchApiTimeout);
 	}
 };
