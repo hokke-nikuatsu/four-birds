7ec0471f6ed05240044faca6b7b5d004df725880
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-23 00:47:01 +0000

    🏷️ Add types

---
+++ frontend/src/types/api.ts
@@ -0,0 +1,21 @@
+export enum STATUS {
+	ERROR = 'error',
+	SUCCESS = 'success',
+}
+
+export type ApiResponse<T> = {
+	status: 'success' | 'error';
+	data?: T;
+	message?: string;
+};
+
+export type FetchNewsResponse = {
+	articleId: string;
+	title: string;
+	description: string;
+	publishedDate: string;
+	url: string;
+	ogpUrl: string;
+	publisherName: string;
+	categories: string[];
+}[];
---
+++ frontend/src/types/components.ts
@@ -0,0 +1,28 @@
+export enum Colors {
+	'Top' = '#d6e4f7',
+	'Technology' = '#FFCDD2',
+	'Business' = '#C8E6C9',
+	'Health' = '#BBDEFB',
+	'Sports' = '#FFECB3',
+	'Crime' = '#E1BEE7',
+	'Domestic' = '#B3E5FC',
+	'Education' = '#F0F4C3',
+	'Entertainment' = '#FFCCBC',
+	'Food' = '#FFF9C4',
+	'Politics' = '#D1C4E9',
+	'Science' = '#C5CAE9',
+	'Tourism' = '#B2EBF2',
+	'Other' = '#F5F5F5',
+	'Environment' = '#DCEDC8',
+	'World' = '#D7CCC8',
+}
+
+export type NewsItem = {
+	title: string;
+	description: string;
+	publishedDate: string;
+	url: string;
+	ogpUrl: string;
+	publisherName: string;
+	categories: string[];
+};
