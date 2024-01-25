cbf44fdacd5b8bbccdeacbcb9ac521833e6819b0
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-25 16:54:13 +0000

    🔥 Remove footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import TopPage from './pages/TopPage/TopPage';
 import './App.css';
@@ -14,7 +13,6 @@ const App: React.FC = () => (
 					<Route path="/" element={<TopPage />} />
 				</Routes>
 			</main>
-			<Footer />
 		</div>
 	</Router>
 );
--- frontend/src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-	<footer>
-		<h1>諸々</h1>
-	</footer>
-);
-
-export default Footer;
