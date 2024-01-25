344660702b5fc570de5b4157747c5a33d85e2c40
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-25 16:55:57 +0000

    👔 Add infinity loading logic

--- frontend/src/components/NewsList/NewsList.tsx
+++ frontend/src/components/NewsList/NewsList.tsx
@@ -1,38 +1,79 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { NewsGrid } from './NewsListStyle';
 import { fetchNews } from '../../services/newsService';
 import { type FetchNewsResponse } from '../../types/api';
+import Loading from '../Loading/Loading';
 import NewsCard from '../NewsCard/NewsCard';
 
-const NewsList = () => {
-	const [newsItems, setNewsItems] = useState<FetchNewsResponse>();
+const NewsList: React.FC = () => {
+	const [newsItems, setNewsItems] = useState<FetchNewsResponse>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [pageIndex, setPageIndex] = useState<number>(0);
 
-	const fetchNewsItems = async (start: number) => {
-		const data = await fetchNews(start);
+	const fetchNewsItems = useCallback(
+		async (start: number) => {
+			if (isLoading) {
+				return;
+			}
 
-		setNewsItems(data);
-	};
+			setIsLoading(true);
+
+			try {
+				const data = await fetchNews(start);
+
+				setNewsItems([...newsItems, ...data]);
+				setPageIndex(pageIndex + 21);
+			} catch (e) {
+				console.error('Fetch news items failed:', e);
+
+				// TODO : Popup error messages
+			} finally {
+				setIsLoading(false);
+			}
+		},
+		[isLoading, newsItems, pageIndex],
+	);
 
 	useEffect(() => {
-		fetchNewsItems(0);
+		fetchNewsItems(pageIndex);
+		// Make empty the second argument to avoid reloading fetchNewsItems multiple times.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	useEffect(() => {
+		const handleScroll = () => {
+			if (
+				window.innerHeight + document.documentElement.scrollTop >=
+				document.documentElement.offsetHeight - 10
+			) {
+				fetchNewsItems(pageIndex);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [fetchNewsItems, pageIndex]);
+
 	return (
-		<NewsGrid>
-			{newsItems &&
-				newsItems.map((newsItem) => (
-					<NewsCard
-						key={newsItem.articleId}
-						title={newsItem.title}
-						description={newsItem.description}
-						publishedDate={newsItem.publishedDate}
-						url={newsItem.url}
-						ogpUrl={newsItem.ogpUrl}
-						publisherName={newsItem.publisherName}
-						categories={newsItem.categories}
-					/>
-				))}
-		</NewsGrid>
+		<>
+			<NewsGrid>
+				{newsItems &&
+					newsItems.map((newsItem) => (
+						<NewsCard
+							key={newsItem.articleId}
+							title={newsItem.title}
+							description={newsItem.description}
+							publishedDate={newsItem.publishedDate}
+							url={newsItem.url}
+							ogpUrl={newsItem.ogpUrl}
+							publisherName={newsItem.publisherName}
+							categories={newsItem.categories}
+						/>
+					))}
+			</NewsGrid>
+			{<Loading isLoading={isLoading} />}
+		</>
 	);
 };
 
