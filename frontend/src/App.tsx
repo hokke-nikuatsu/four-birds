0ccb9c57eae3ee19d1c61937ff07236f86aa08fc
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-31 21:10:35 +0000

    👔 Set user authenticator in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import TopPage from './pages/TopPage/TopPage';
 import './App.css';
+import { UserAuthenticator } from './services/auth/userAuthenticator';
 import { store } from './services/store/store';
+
+export const userAuthenticator = new UserAuthenticator();
 
 const App = () => (
 	<Provider store={store}>
