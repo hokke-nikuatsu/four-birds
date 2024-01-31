afcbef54499b84f6176af272feb62fb942028dcc
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-31 21:10:15 +0000

    👔 Add user and menu in header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,25 @@
 import { Link } from 'react-router-dom';
-import { StyledHeader, LogoImage } from './HeaderStyle';
+import {
+	StyledHeader,
+	LogoImage,
+	MenuButton,
+	Spacer,
+	MenuIcon,
+} from './HeaderStyle';
+import User from './User/User';
+import { PATH_TO_LOGO_IMAGE, PATH_TO_MENU_ICON } from '../../utils/common';
 
 const Header = () => (
 	<StyledHeader>
+		<MenuButton>
+			<MenuIcon src={PATH_TO_MENU_ICON} alt={'Menu icon'} />
+		</MenuButton>
+		<Spacer />
 		<Link to="/">
 			<LogoImage src={PATH_TO_LOGO_IMAGE} alt="Logo" />
 		</Link>
+		<Spacer />
+		<User />
 	</StyledHeader>
 );
 
