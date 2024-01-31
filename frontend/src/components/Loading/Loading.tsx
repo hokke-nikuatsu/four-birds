15ab71fa69211bbd8314203db849b3d4874aa9c1
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-01-31 21:05:49 +0000

    👔 Add loading icon below news

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,9 +1,9 @@
 import CircularProgress from '@mui/material/CircularProgress';
-import { LoadingArea } from './LoadingStyle';
+import { StyledLoading } from './LoadingStyle';
 import { type Loading as LoadingType } from '../../types/components';
 
 const Loading: React.FC<LoadingType> = ({ isLoading }) => (
-	<LoadingArea>{isLoading && <CircularProgress />}</LoadingArea>
+	<StyledLoading>{isLoading && <CircularProgress />}</StyledLoading>
 );
 
 export default Loading;
