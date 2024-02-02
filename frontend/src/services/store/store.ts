fd618eeb577474daaca94873d5792dd63e59d893
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-02 23:11:17 +0000

    👔 Add thunk to store

--- frontend/src/services/store/store.ts
+++ frontend/src/services/store/store.ts
@@ -1,11 +1,22 @@
-import { compose, createStore } from 'redux';
+import { useDispatch } from 'react-redux';
+import { type Action, applyMiddleware, compose, createStore } from 'redux';
+import { type ThunkDispatch, thunk } from 'redux-thunk';
 import { ENV } from '../../utils/environment';
 import { reducers } from '../redux/rootReducer';
 
 const shouldDisableDevTools = ENV === 'production';
 
+const middleware = [thunk];
+
 const composeEnhancers =
 	(!shouldDisableDevTools && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
 	compose;
 
-export const store = createStore(reducers, composeEnhancers());
+export const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(...middleware)),
+);
+
+export type AppState = ReturnType<typeof reducers>;
+export type AppDispatch = ThunkDispatch<AppState, unknown, Action>;
+export const useAppDispatch: () => AppDispatch = useDispatch;
