8abe8a7ab818047e3201a8e27e7ce28ab9f52ee4
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-23 21:08:28 +0000

    👔 Add generate csrf-token logic

--- frontend/src/services/api/users.ts
+++ frontend/src/services/api/users.ts
@@ -32,3 +32,52 @@ export const updateUser = async (user: User): Promise<ApiResponse<void>> => {
 	}
 };
 
+export const generateCsrfToken = async (
+	user: User,
+): Promise<ApiResponse<void>> => {
+	try {
+		const response = await fetch(`${API_URL}/user/${user.uid}`, {
+			method: 'GET',
+			credentials: 'include',
+		});
+
+		if (!response.ok) {
+			throw new Error(`Error: ${response.status}`);
+		}
+
+		const generateCsrfTokenResponse: ApiResponse<void> = await response.json();
+
+		if (generateCsrfTokenResponse.status !== STATUS.SUCCESS) {
+			throw generateCsrfTokenResponse;
+		}
+
+		return generateCsrfTokenResponse;
+	} catch (e) {
+		console.error('Generate CSRF token failed:', JSON.stringify(e));
+
+		throw e;
+	}
+};
+
+export const deleteCsrfToken = async (user: User): Promise<void> => {
+	try {
+		const response = await fetch(`${API_URL}/user/${user.uid}`, {
+			method: 'DELETE',
+			credentials: 'include',
+		});
+
+		if (!response.ok) {
+			throw new Error(`Error: ${response.status}`);
+		}
+
+		const deleteTokenResponse: ApiResponse<void> = await response.json();
+
+		if (deleteTokenResponse.status !== STATUS.SUCCESS) {
+			throw deleteTokenResponse;
+		}
+	} catch (e) {
+		console.error('Delete CSRF token failed:', JSON.stringify(e));
+
+		throw e;
+	}
+};
