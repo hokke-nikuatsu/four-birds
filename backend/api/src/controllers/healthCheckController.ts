d3b26e3be1c07ad93e6150dc404650ff1edcc759
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-23 21:12:54 +0000

    🥅 Add try catch

--- backend/api/src/controllers/healthCheckController.ts
+++ backend/api/src/controllers/healthCheckController.ts
@@ -1,5 +1,14 @@
 import { type Request, type Response } from 'express';
+import { STATUS } from '../types/io';
 
 export const healthCheck = async (_req: Request, res: Response) => {
-	res.sendStatus(200);
+	try {
+		res.sendStatus(200);
+	} catch (e) {
+		if (e instanceof Error) {
+			res.status(500).send({ status: STATUS.ERROR, message: e.message });
+		} else {
+			res.status(500).send({ status: STATUS.ERROR, message: 'Unknown error.' });
+		}
+	}
 };
