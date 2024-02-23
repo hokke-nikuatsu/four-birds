d53ce6fff85849299f2daecbe9ee4831e4093668
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-23 21:12:32 +0000

    👔 Modify api route and middleware

--- backend/api/src/app.ts
+++ backend/api/src/app.ts
@@ -1,9 +1,11 @@
-import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import articleRoutes from './routes/articleRoutes';
+import articleRoute from './routes/articleRoute';
 import healthCheckRoute from './routes/healthCheckRoute';
-import { API_HEALTH_CHECK, API_ARTICLE } from './utils/api';
+import userRoute from './routes/userRoute';
+import { API_HEALTH_CHECK, API_ARTICLE, API_USER } from './utils/api';
+import { errorHandler } from './utils/common';
 import { ALLOWED_ORIGIN } from './utils/env';
 
 const app = express();
@@ -11,12 +13,17 @@ const app = express();
 app.use(
 	cors({
 		origin: ALLOWED_ORIGIN,
+		credentials: true,
 	}),
 );
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(API_ARTICLE, articleRoutes);
+app.use(API_ARTICLE, articleRoute);
+app.use(API_USER, userRoute);
 app.use(API_HEALTH_CHECK, healthCheckRoute);
+
+app.use(errorHandler);
 
 export default app;
