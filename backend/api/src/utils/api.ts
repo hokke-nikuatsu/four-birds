ec8dd08ed46b93efb68d2271254f9fdf4286c5dd
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-23 21:14:59 +0000

    👔 Change API path

--- backend/api/src/utils/api.ts
+++ backend/api/src/utils/api.ts
@@ -1,2 +1,5 @@
-export const API_ARTICLE = '/api/article';
-export const API_HEALTH_CHECK = '/api/health-check';
+const API_ROOT = '/api';
+export const API_ARTICLE = `${API_ROOT}/article`;
+export const API_USER = `${API_ROOT}/user/:userId`;
+export const API_TOKEN = `${API_ROOT}/token`;
+export const API_HEALTH_CHECK = `${API_ROOT}/health-check`;
