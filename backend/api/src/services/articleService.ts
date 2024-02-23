d7873d61ffbe6c95ba1e57259d49a983754902e8
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-02-23 21:13:34 +0000

    🥅 Add try catch

--- backend/api/src/services/articleService.ts
+++ backend/api/src/services/articleService.ts
@@ -15,29 +15,37 @@ export const fetchArticles = async (
 	connection: PoolConnection,
 	offSet: number,
 ): Promise<FetchArticlesResponse> => {
-	const result = await connection.query(QUERY_SELECT_ARTICLES, [offSet]);
-	const [rows] = result as RowDataPacket[];
+	try {
+		const result = await connection.query(QUERY_SELECT_ARTICLES, [offSet]);
+		const [rows] = result as RowDataPacket[];
 
-	if (!rows) {
-		throw new Error('Fetching articles failed.');
+		if (!rows) {
+			throw new Error('Fetching articles failed.');
+		}
+
+		const articles = rows.map(convertToCamelCase) as DBFetchArticles;
+
+		const formattedArticles = articles.map((article) => {
+			const convertedPublishedDate = convertToJapanTime(article.publishedDate);
+			const formattedPublishedDate = formatDate(convertedPublishedDate);
+			const trimmedDescription = trimText(article.description);
+			const splittedCategories = splitText(article.categories);
+			const formattedCategories = capitalizeFirstLetter(splittedCategories);
+
+			return {
+				...article,
+				description: trimmedDescription,
+				publishedDate: formattedPublishedDate,
+				categories: formattedCategories,
+			};
+		});
+
+		return formattedArticles;
+	} catch (e) {
+		await connection.rollback();
+
+		throw new Error(`Fetch articles failed: ${e}`);
+	} finally {
+		connection.release();
 	}
-
-	const articles = rows.map(convertToCamelCase) as DBFetchArticles;
-
-	const formattedArticles = articles.map((article) => {
-		const convertedPublishedDate = convertToJapanTime(article.publishedDate);
-		const formattedPublishedDate = formatDate(convertedPublishedDate);
-		const trimmedDescription = trimText(article.description);
-		const splittedCategories = splitText(article.categories);
-		const formattedCategories = capitalizeFirstLetter(splittedCategories);
-
-		return {
-			...article,
-			description: trimmedDescription,
-			publishedDate: formattedPublishedDate,
-			categories: formattedCategories,
-		};
-	});
-
-	return formattedArticles;
 };
