490e5b1140ff9629ada31806b878bd9c032379a9
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-04-24 20:36:26 +0100

    🚚 Change file path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import TopPage from './pages/TopPage/TopPage';
 import './App.css';
-import TrainingPage from './pages/Training/TrainingPage';
+import TrainingPage from './pages/TrainingPage/TrainingPage';
 import { UserAuthenticator } from './services/auth/userAuthenticator';
 import { store } from './services/store/store';
 
