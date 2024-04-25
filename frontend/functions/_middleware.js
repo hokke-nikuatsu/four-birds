0ec9452ab5be89e7f41ddd35bcd353dd9bbe0dfd
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-04-25 22:42:02 +0100

    👷 Add .env to access env vars correctly

--- frontend/functions/_middleware.js
+++ frontend/functions/_middleware.js
@@ -55,13 +55,13 @@ async function handleRequest({ next, request }) {
 		const user = decoded.substring(0, index);
 		const pass = decoded.substring(index + 1);
 
-		if (ENV.BASIC_USER !== user) {
+		if (env.BASIC_USER !== user) {
 			return new Response('Invalid credentials.', {
 				status: 401,
 			});
 		}
 
-		if (ENV.BASIC_PASS !== pass) {
+		if (env.BASIC_PASS !== pass) {
 			return new Response('Invalid credentials.', {
 				status: 401,
 			});
