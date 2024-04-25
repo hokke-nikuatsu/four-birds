4a73a23ffcdd4e4c27c67f68ace4f915799fa7d7
shoot-hokke-dev <shuto.hoshi126.dev@gmail.com>
2024-04-25 22:27:21 +0100

    👷 Remove redundant declaration

--- frontend/functions/_middleware.js
+++ frontend/functions/_middleware.js
@@ -6,8 +6,6 @@
  * A user-id containing a colon (":") character is invalid, as the
  * first colon in a user-pass string separates user and password.
  */
-const BASIC_USER = BASIC_USER;
-const BASIC_PASS = BASIC_PASS;
 
 async function errorHandling(context) {
 	try {
